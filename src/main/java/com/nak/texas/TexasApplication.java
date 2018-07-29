d33aa58df24375cfceca2a900b3aa1c17bdd6d3c
nak-tera <teranaka@DESKTOP-IL4LQDF.localdomain>
2018-07-29 15:45:21 +0900

    initialize spring boot setting

---
+++ src/main/java/com/nak/texas/TexasApplication.java
@@ -0,0 +1,12 @@
+package com.nak.texas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TexasApplication.class, args);
+	}
+}
