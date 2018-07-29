dbe30ccabab33a2ae81c8cb36a8508308a7601e3
nur3478 <kikuchi_tsubasa@yahoo.co.jp>
2018-07-29 21:56:26 +0900

    外枠

---
+++ src/main/java/com/nak/texas/game/cards/check/FourOfAKindChecker.java
@@ -0,0 +1,13 @@
+package com.nak.texas.game.cards.check;
+
+import com.nak.texas.game.Cards;
+
+public class FourOfAKindChecker implements BaseHandChecker {
+
+    @Override
+    public boolean check(Cards[] cards) {
+        // TODO 自動生成されたメソッド・スタブ
+        return false;
+    }
+
+}
