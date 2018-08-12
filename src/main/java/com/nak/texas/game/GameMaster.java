6924cc4346fbb2a8f98f7b2cd990f7a32e07db3b
Sotaro <Sotaro@DESKTOP-DQUUJG2>
2018-08-12 09:39:57 +0900

    フォーム送信処理を作成 人数を入力して他画面に表示

--- src/main/java/com/nak/texas/game/GameMaster.java
+++ src/main/java/com/nak/texas/game/GameMaster.java
@@ -1,10 +1,28 @@
 package com.nak.texas.game;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+/**
+ * Formクラスとしても使用。多分あとで分けたほうがいい。
+ * ゲーム内の処理を行う。
+ * @author tsubasa
+ * @update sugar
+ *
+ */
 public class GameMaster {
 
-    private int playerNum;
+	//Form変数。
+	//TODO クラスを分けたほうがいい
+	@Min(2)
+	@Max(10)
+    private int playerNum = 0;		//とりあえずプレイヤーは2～10人までとした。
     private Player[] players;
 
+    /**
+     * 初期作成時の設定
+     * @param playerNum
+     */
     public GameMaster(int playerNum) {
         this.playerNum = playerNum;
         this.players = new Player[this.playerNum];
@@ -15,17 +33,20 @@ public class GameMaster {
         }
     }
 
+    //TODO 未使用
     public void gameStart() {
         for (int i = 0; i < playerNum; i++) {
             players[i] = new Player();
         }
     }
 
+    //TODO 未使用
     public boolean isGameEnd() {
         // TODO: xxx
         return false;
     }
 
+    //TODO 未使用
     public Player getWinner() {
         Player winner = players[0];
         for (int i = 1; i < playerNum; i++) {
@@ -35,4 +56,15 @@ public class GameMaster {
         }
         return winner;
     }
+
+    //////////////////////////////////////////////////////
+    // getter and setter
+    //////////////////////////////////////////////////////
+	public int getPlayerNum() {
+		return playerNum;
+	}
+
+	public void setPlayerNum(int playerNum) {
+		this.playerNum = playerNum;
+	}
 }
