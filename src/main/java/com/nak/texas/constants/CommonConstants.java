0eab481066152575370d5a6b36a229dfa780c063
Sotaro <Sotaro@DESKTOP-DQUUJG2>
2018-08-12 16:23:30 +0900

    コンスタンツクラスのコミット忘れ
    定数を設定するためのクラスを新規作成したが、コミットが漏れていた

---
+++ src/main/java/com/nak/texas/constants/CommonConstants.java
@@ -0,0 +1,14 @@
+package com.nak.texas.constants;
+
+/**
+ * sugar所属会社のNNS開発標準にあったため作成。
+ * ハードコードを避けるため、Javaクラス内で使用している固定値はここで設定したものを呼び出す。
+ * @author Sotaro
+ *
+ */
+public class CommonConstants {
+	public static String ParameterGameMaster = "gameMaster";
+	public static String PathGame = "/game";
+	public static String ParameterPlayerNum = "playerNum";
+	public static String PathResult = "/result";
+}
